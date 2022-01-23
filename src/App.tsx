96c33d10bc3a8820c886b450103ad5cf384f2b3d
Artem <leskevichtema@gmail.com>
2022-01-23 21:20:05 +0300

    lesson3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Lesson3 from "./lessons/lesson3/Lesson3";
 // import Lesson1 from './lessons/lesson1/lesson1';
 // import './lessons/lesson2/lesson2';
 // import Lesson3 from './lessons/lesson3/Lesson3';
@@ -13,7 +14,7 @@ function App() {
     return (
         <div className="container">
             {/*<Lesson1/>*/}
-            {/*<Lesson3 />*/}
+            <Lesson3 />
             {/*<Lesson4 />*/}
         </div>
     );
